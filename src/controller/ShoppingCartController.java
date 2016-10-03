82dcbf09b686d1286ca1a67dff4770afbadf56ce
Wint3rNuk3 <michaeldanielcarrasco@gmail.com>
2016-10-03 15:03:36 +0200

    dossiers CSS et IMAGES.

--- src/controller/ShoppingCartController.java
+++ src/controller/ShoppingCartController.java
@@ -2,7 +2,6 @@
 package controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -20,18 +19,6 @@ public class ShoppingCartController extends HttpServlet {
         
         HttpSession session = request.getSession();
         
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet shoppingCartController</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet shoppingCartController at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
--- src/model/classes/ShoppingCart.java
+++ src/model/classes/ShoppingCart.java
@@ -4,4 +4,21 @@ import java.util.HashMap;
 
 public class ShoppingCart {
     private HashMap<String, Ouvrage> map;
+
+    public ShoppingCart() {
+        this.map = new HashMap();
+    }
+    
+    public ShoppingCart(HashMap<String, Ouvrage> map) {
+        this.map = map;
+    }
+
+    public HashMap<String, Ouvrage> getMap() {
+        return map;
+    }
+
+    public void setMap(HashMap<String, Ouvrage> map) {
+        this.map = map;
+    }
+    
 }

a531a1393cff4226291b1cdf02f0e6e55e54e25c
Marine <marineg0108@gmail.com>
2016-10-04 09:44:47 +0200

    commit de debut !
    Rien de special juste création de classe et autres

--- src/controller/OrderController.java
+++ src/controller/OrderController.java
@@ -1,61 +1,33 @@
+
 package controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import model.beans.ShoppingCartBean;
 
-@WebServlet(name = "shoppingCartController", urlPatterns = {"/shoppingCartController"})
-public class ShoppingCartController extends HttpServlet {
+@WebServlet(name = "OrderController", urlPatterns = {"/OrderController"})
+public class OrderController extends HttpServlet {
 
+    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-
-        HttpSession session = request.getSession();
-        // get le panier à partir de la session.
-        ShoppingCartBean cart = (ShoppingCartBean) session.getAttribute("ShoppingCart");
-
-        // Si l'utilisateur n'a pas de panier, le creer.
-        if (cart == null) {
-            cart = new ShoppingCartBean();
-            session.setAttribute("ShoppingCart", cart);
-        }
-
-        if (request.getParameter("add") != null) {
-            cart.create(request.getParameter("add"));
-        }
-        if (request.getParameter("inc") != null) {
-            cart.inc(request.getParameter("inc"));
-        }
-        if (request.getParameter("dec") != null) {
-            cart.dec(request.getParameter("dec"));
-        }
-        if (request.getParameter("del") != null) {
-            cart.del(request.getParameter("del"));
-        }
-        if (request.getParameter("clean") != null) {
-            cart.clean();
-        }
-        
-        
-        
-        // à l'arrache pour le moment
-        
-        if(request.getParameter("save") != null){
-            session.setAttribute("ShoppingCart", cart);
-        }
-        if(request.getParameter("load") != null){
-            ShoppingCartBean cartL = (ShoppingCartBean) session.getAttribute("ShoppingCart"); // faut il le mettre dans un objet ? sais pas. 
-                                                                                              // tu le sais toi ? moi non.
-                                                                                              // moi non plus alors...fait le au feeling ! 
+        try (PrintWriter out = response.getWriter()) {
             
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet OrderController</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet OrderController at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
         }
-
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
